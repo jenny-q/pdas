18b05b110812be2bfea93bbceb3df2984723d63b
Jennifer Quispe <jenny.q@me.com>
2022-12-01 14:36:06 -0500

    updating child props, adding bulma css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from "./ProfileCard";
+import AlexaImg from './images/alexa.png'
+import CortanaImg from './images/cortana.png'
+import SiriImg from './images/siri.png'
 
 function App() {
     return (
         <section>
-            <div>
-                <h1>Personal Digital Assistants</h1>
+            <div className='hero is-primary'>
+                <div className='hero-body'>
+                    <h1 className='title'>Personal Digital Assistants</h1>
+                </div>
             </div>
-            <ProfileCard title="alexa" handle="@alexa99" />
-            <ProfileCard title="cortana" handle= "@cortana69"/>
-            <ProfileCard title="siri" handle="@siri28"/>
+
+            <div className="container">
+                <div className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard title="alexa" handle="@alexa99" img={AlexaImg} />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="cortana" handle= "@cortana69" img={CortanaImg}/>
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="siri" handle="@siri28" img={SiriImg}/>
+                        </div>
+                    </div>
+                </div>
+            </div>      
         </section>
     )
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,11 +1,20 @@
-function ProfileCard({title, handle}) {
+function ProfileCard({title, handle, img}) {
     // const title = props.title
     // const handle = props.handle
     // const {title, handle} = props;
     return (
-        <div>
-            <h2>title: {title}</h2>
-            <p>{handle}</p>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={img} alt="" />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+            </div>
         </div>
     )
 }
